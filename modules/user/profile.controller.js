1256ed58f37c44a49b7ac7b501decaebfeabe40c
Abdullah Bashir <mabdullah353@gmail.com>
2017-10-24 00:02:08 +0500

    updated verion number

--- modules/user/profile.controller.js
+++ modules/user/profile.controller.js
@@ -3,18 +3,30 @@
 
   var app = angular.module('user');
 
-  app.controller('ProfileController', ['$scope', '$stateParams', 'UserService',
-    function($scope, $stateParams, UserService) {
-      var user = UserService.getById($stateParams.userID);
-      $scope.id = user.id;
-      $scope.firstName = user.firstName;
-      $scope.lastName = user.lastName;
-      $scope.email = user.email;
-      $scope.country = user.country;
-      $scope.dateOfBirth = user.dateOfBirth;
+  app.controller('ProfileController', 
+    ['$rootScope', '$scope', '$stateParams', 'UserService',
+    function($rootScope, $scope, $stateParams, UserService) {
 
+      var user = null;
       $scope.editMode = false;
       
+      var showUser = function(id) {
+        user = UserService.getById(id);
+        if(!user) { return; }
+
+        $scope.id = user.id;
+        $scope.firstName = user.firstName;
+        $scope.lastName = user.lastName;
+        $scope.email = user.email;
+        $scope.country = user.country;
+        $scope.dateOfBirth = user.dateOfBirth;      
+      };
+
+      if (UserService.all()) { showUser($stateParams.userID); }
+      $rootScope.$on('UserService:usersAvailable', function() {
+        showUser($stateParams.userID);
+      });
+
       $scope.editUser = function() {
         $scope.editMode = !$scope.editMode;
       };
